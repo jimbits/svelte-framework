8c67ac770440d58c1abd57ae1b7189bcbb9d143c
jimbits <contactjamesfoley@gmail.com>
2020-08-05 22:11:34 -0600

    added product card component

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,7 @@
 <script>
 	import Card from './Card.svelte'
 </script>
-
-
-		<h1>Seeds For Sale</h1>
+ 
 		<Card/>
 <style>
 </style>
--- src/Card.svelte
+++ src/Card.svelte
@@ -1,28 +1,176 @@
-<script>
 
-</script>
 
-<style>
+<div class="card">
+  <header class="strain">
+    <h1 class="strain-name">Jack Herer</h1>
+    <div class="seed-price">
+      <p class="price">
+        $10.00
+        <sup>CDN</sup>
+      </p>
 
-</style>
-
-<div>
-
-  <header>
-    <ul>
-      <li>Strain Wedding</li>
-      <li>Breeder: Seedsman</li>
-      <li>Seed Price: $12.00</li>
-    </ul>
-
+    </div>
+ 
   </header>
+ 
+   
+  <img class="photo"
+  src="https://www.seedsman.com/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/i/m/img_9084_jh_d76_2000.jpg"
+  alt="flowers" />
 
-  <img
-    src="https://www.seedsman.com/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/w/e/wedding-cake-auto.jpg"
-    alt="wedding cake" />
+  <section class="characterisitics">
+    <h2 class="card-heading">Characteristics</h2>
+    <table>
+      <tr>
+        <td>Breeder</td>
+        <td>Seedsman</td>
+      </tr>
+      <tr>
+        <td>Genetics</td>
+        <td>Jack Herer 25% x Ruderalis 75%</td>
+      </tr>
+      <tr>
+        <td>Flowering Type</td>
+        <td>Femanized</td>
+      </tr>
+      <tr>
+        <td>THC Content</td>
+        <td>average is between 16% and 22%</td>
+      </tr>
+      <tr>
+        <td>CBD Content</td>
+        <td>less than 0.5%</td>
+      </tr>
+      <tr>
+        <td>Yield Indoors</td>
+        <td>
+          400 grams per plant
+   
+        </td>
+      </tr>
+      <tr>
+        <td>Plant Height</td>
+        <td>&lt; 100cm or 3ft</td>
+      </tr>
+      <tr>
+        <td>Taste/Flavour</td>
+        <td>Pepper, Sweet, Citrus Flavours</td>
+      </tr>
+    </table>
+  </section>
 
-  <a href="https://www.seedsman.com/en/wedding-cake-auto-feminised-seeds-5">
-    wedding cake
-  </a>
+ 
+
+    <section class="details">
+
+      <h2 class="card-heading">Details</h2>
+
+      <p>
+        The Jack Herer Fast strain is a 75% sativa-dominant strain. You will find it resistant to most diseases if grown in correctly. This strain produces some pretty good yields. As a novice grower I was able to obtain about 4oz or 120grams per plant. 
+      </p>
+ 
+        <h3 class="detail-heading">Veg</h3>
+      <p>
+        When grown indoors from seed you can expect the plant to reach between 90 - 100 cm  tall, that's about 3ft for those of us from the old school. If you give the strain a little less blue light and more red in the last few weeks of veg or during the transition to flower you can will reveal more of the sativa dominat charachterisics from the plant. Depending on how you grow you can expect the strain to be in veg for 2 months assuming your using common low/high stress training techiniques. You can flip it to veg earlier at about week six if your not training your plants.    
+      </p>
+      <h3 class="detail-heading">Flower</h3>
+      <p>Though you will find that the flowering period for this strain is listed at 8 to 9 weeks you might find that you will benifit from another month. Growers find that the transition to 70% cloudy trichomes isn't completed unitl later in the flowering process. So give yourself some extra time and let it slowly rippen</p>
+
+      <h3 class="detail-heading">Potency</h3>
+      <p> Jack Herer has a sweet fruity, peppery taste. Its THC production is in the range of  16% to 22%. CBD levels are low maxing out at 0.5%.
+        giving a potent effect.</p>
+        <h3 class="detail-heading">Terpen Profile</h3>
+        <p>The strain has a high production of Myrcene and Limonene, which gives the strain some fruity notes along with some interesting citrus overtones.  
+        </p>
+      <h3 class="detail-heading">Yields</h3>
+      <p>
+        Depending on your expertise some growers report upward of 700grams per plant or 7 ounces.
+      </p>
+      <h3 class="detail-heading">Effect</h3>
+      <p>The plant produces an energetic and uplifting effect that allows you to get stuff done. The effect is long lasting and transitions nicely to a relaxing  peaceful calm state of being. </p>
+    
+      <button>Seeds</button>
+      <button>Wishlist</button>
+    </section>
+ 
+
 </div>
 
+<style>
+  .card {
+    box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.05);
+    font-family: "product";
+  }
+  h2 {
+    font-size: 1.5rem;
+ 
+  }
+  h3{
+    font-size:1.125rem;
+  }
+
+  .strain {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.5rem;
+  }
+
+  .seed-price {
+    text-align: center;
+ 
+  }
+  .price {
+    font-size: 2.5rem;
+  }
+  .price sup {
+    font-size: 12px;
+    margin-left: -0.5rem;
+    font-weight: bold;
+  }
+
+
+  .characterisitics {
+    margin: 2rem;
+  }
+  
+  table {
+    border-collapse: separate;
+    table-layout: auto;
+    width: 100%;
+    font-family: "product";
+ 
+    border-spacing:0 0.125rem;
+  }
+
+  tr {
+    border-spacing: 1rem;
+    text-align: left;
+   
+  }
+  td{
+    padding:0.25rem;
+  ;
+  }
+  td:nth-child(odd) {
+    color:white;
+    background:#92a9ae;;
+    text-align: right;
+    padding-right: 0.5rem;
+    
+  }
+  td:nth-child(even) {
+    border-right:none;
+    padding-left: 0.5rem;
+    background:#ccdbdb;
+    
+  }
+
+  .details{
+    margin: 2rem;
+  }
+  p{
+    margin-bottom:1.5rem;
+    color:rgb(128, 126, 126);
+  }
+</style>
